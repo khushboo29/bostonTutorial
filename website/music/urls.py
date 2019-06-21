182c0322eada511d813d52243d877519a6ed0550
Khushboo Tiwari <ktiwari1991@gmail.com>
2019-06-21 18:29:39 +0530

    adding namespace in urls

--- website/music/urls.py
+++ website/music/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.urls import include, path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     path('<int:album_id>/', views.detail, name="detail")
